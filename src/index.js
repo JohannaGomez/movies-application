10aa043ae6898e37b12b9998654da1cafdd8eb01
Johanna Gomez <pao_gomez1@hotmail.com>
2017-11-15 14:10:31 -0600

    adding movies done, editing movies done, deleting movies done

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ getMovies().then((movies) => {
     });
   buildHtml(movies);
   editMovies(movies);
+  deleteMovies(movies);
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
@@ -31,7 +32,7 @@ function buildHtml(arrObjs) {
         const {title, rating, id} = movie;
         arrObjsHtml += `<tr id="movie-${id}"><th>${title}</th><th>${rating}</th><th>${id}</th>
                         <th><button class="btn btn-primary" id="editMovieBtn${movie.id}">Edit</button></th>
-                        <th><button class="btn btn-primary" id="deleteMovieBtn${movie.id}">Delete</button></th></tr>
+                        <th><button class="btn btn-danger" id="deleteMovieBtn${movie.id}">Delete</button></th></tr>
                         <div id="editMovieRow-${id}"></div>`;
     });
     $("#insertMovies").html(arrObjsHtml);
@@ -71,32 +72,77 @@ function editMovies(arrObjs) {
         $(`#editMovieBtn${movie.id}`).click(function () {
             $('#editingMovies').toggleClass('invisible');
             editHtml = `<input id="edit_movie_name" type="text" value="${movie.title}">  Rating: ${movie.rating}<div id="tempID">${movie.id}</div>`;
-            // console.log(editHtml);
             $("#print-edited-movies").html(editHtml);
         });
 
     });
     $("#editingMovieBtn").click(function () {
-        console.log($('#edit_movie_name').val());
-        // const moviePut = {title: $('#edit_movie_name').val() , rating: $('#edit_rating_value').val() };
-        // const id = $('#tempID').html();
-        // console.log(moviePut);
-        // const url = `/api/movies/${id}`;
-        // const options = {
-        //     method: 'PUT',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(moviePut)
-        // };
-        //
-        // fetch(url, options)
-        //     .then(console.log("fetch for editing movies"))
-        //     .catch(console.log("error for editing movies"));
+        const moviePut = {title: $('#edit_movie_name').val() , rating: $('#edit_rating_value').val() };
+        const id = $('#tempID').html();
+        console.log(moviePut);
+        const url = `/api/movies/${id}`;
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(moviePut)
+        };
+
+        fetch(url, options)
+            .then(() => {
+
+                getMovies().then((movies) => {
+                    const $loading = $('#loading').hide();
+                    $(document)
+                        .ajaxStart(function () {
+                            $loading.hide('body');
+                        })
+                        .ajaxStop(function () {
+                            $loading.show('body');
+                        });
+                    // console.log('Here are all the movies:');
+                    movies.forEach(({title, rating, id}) => {
+                        console.log(`id#${id} - ${title} - rating: ${rating}`);
+                    });
+                    buildHtml(movies);
+                    editMovies(movies);
+                    deleteMovies(movies);
+                }).catch((error) => {
+                    alert('Oh no! Something went wrong.\nCheck the console for details.')
+                    console.log(error);
+                });
+
+            })
+            .catch(console.log("error for editing movies"));
     })
 
 };
 
+
+// ================================== Function to delete movies ==============================
+
+function deleteMovies(arrObjs) {
+    arrObjs.forEach(function (movie, index, array) {
+        const {title, rating, id} = movie;
+        $(`#deleteMovieBtn${movie.id}`).click(function () {
+            console.log("delete button clicked");
+            const movieDelete = {title: movie.title, rating: movie.rating, id: movie.id};
+            const url = `/api/movies/${id}`;
+            const options = {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(movieDelete)
+            };
+
+            fetch(url, options)
+                .then(console.log("fetch for editing movies"))
+                .catch(console.log("error for editing movies"));
+        })
+    });
+};
 
 
 
